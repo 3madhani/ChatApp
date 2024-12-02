ec0cac7a93265dab16468b47917a5bd35e09a807
3mad Hany <144627820+3madhani@users.noreply.github.com>
2024-12-02 14:10:21 +0200

    push changes

--- lib/views/chat_screen.dart
+++ lib/views/chat_screen.dart
@@ -17,140 +17,143 @@ class ChatScreen extends StatelessWidget {
 
   TextEditingController controller = TextEditingController();
 
-  List<Message> messageList = [
-    Message(
-      'i am jon',
-      '1',
-    ),
-    Message(
-      'i am Emad',
-      '1',
-    ),
-    Message(
-      'Hi',
-      '1',
-    ),
-    Message(
-      'Hi',
-      '1',
-    ),
-  ];
-
-  List<Message> messageList2 = [];
   @override
   Widget build(BuildContext context) {
     var email = ModalRoute.of(context)!.settings.arguments;
-    return SafeArea(
-      child: Scaffold(
-        drawer: Drawer(
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-            const Spacer(
-              flex: 1,
-            ),
-            FilledButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, LoginScreen.id);
-              },
-              child: const Text(
-                'Logout',
-                style: TextStyle(
-                  fontSize: 18,
+    return StreamBuilder<QuerySnapshot>(
+      stream: message.orderBy(time, descending: true).snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<Message> messagesList = [];
+          for (int i = 0; i < snapshot.data!.docs.length; i++) {
+            messagesList.add(
+              Message.fromJson(
+                snapshot.data!.docs[i],
+              ),
+            );
+          }
+          return SafeArea(
+            child: Scaffold(
+              drawer: Drawer(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const Spacer(
+                        flex: 1,
+                      ),
+                      FilledButton(
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(
+                              context, LoginScreen.id);
+                        },
+                        child: const Text(
+                          'Logout',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ]),
+              ),
+              appBar: AppBar(
+                bottomOpacity: 990,
+                elevation: 50,
+                backgroundColor: Colors.purple[288],
+                automaticallyImplyLeading: false,
+                centerTitle: true,
+                title: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'ChatApp',
+                      style: TextStyle(
+                        color: Colors.deepPurple,
+                        fontSize: 32,
+                        fontWeight: FontWeight.w800,
+                        // fontFamily: 'Pacifico''
+                      ),
+                    )
+                  ],
                 ),
               ),
-            ),
-          ]),
-        ),
-        appBar: AppBar(
-          bottomOpacity: 990,
-          elevation: 50,
-          backgroundColor: Colors.purple[288],
-          automaticallyImplyLeading: false,
-          centerTitle: true,
-          title: const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'ChatApp',
-                style: TextStyle(
-                  color: Colors.deepPurple,
-                  fontSize: 32,
-                  fontWeight: FontWeight.w800,
-                  // fontFamily: 'Pacifico''
-                ),
-              )
-            ],
-          ),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                reverse: true,
-                controller: scrollController,
-                itemCount: messageList.length,
-                itemBuilder: (context, index) {
-                  return index % 2 != 0
-                      ? ChatBubbles(
-                          message: messageList[index],
-                        )
-                      : ChatBubblesForFriend(
-                          message: messageList[index],
-                        );
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(14.0),
-              child: TextField(
-                controller: controller,
-                onSubmitted: (data) {
-                  message.add(
-                    {
-                      kMessage: data,
-                      time: DateTime.now(),
-                      'id': email,
-                    },
-                  );
-                  controller.clear();
-                  scrollController.animateTo(
-                    0,
-                    duration: const Duration(milliseconds: 500),
-                    curve: Curves.easeIn,
-                  );
-                },
-                decoration: InputDecoration(
-                    hintText: 'send message',
-                    suffixIcon: IconButton(
-                      onPressed: () {
+              body: Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      reverse: true,
+                      controller: scrollController,
+                      itemCount: messagesList.length,
+                      itemBuilder: (context, index) {
+                        return messagesList[index].id == email
+                            ? ChatBubbles(
+                                message: messagesList[index],
+                              )
+                            : ChatBubblesForFriend(
+                                message: messagesList[index],
+                              );
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(14.0),
+                    child: TextField(
+                      controller: controller,
+                      onSubmitted: (data) {
                         message.add(
                           {
-                            kMessage: controller.text,
+                            kMessage: data,
                             time: DateTime.now(),
                             'id': email,
                           },
                         );
                         controller.clear();
+                        scrollController.animateTo(
+                          0,
+                          duration: const Duration(milliseconds: 500),
+                          curve: Curves.easeIn,
+                        );
                       },
-                      icon: const Icon(
-                        Icons.send,
-                      ),
+                      decoration: InputDecoration(
+                          hintText: 'send message',
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              message.add(
+                                {
+                                  kMessage: controller.text,
+                                  time: DateTime.now(),
+                                  'id': email,
+                                },
+                              );
+                              controller.clear();
+                            },
+                            icon: const Icon(
+                              Icons.send,
+                            ),
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: const BorderSide(
+                                width: 2,
+                                color: Colors.deepPurple,
+                              )),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: const BorderSide(
+                                  color: Colors.deepPurple, width: 2))),
                     ),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                        borderSide: const BorderSide(
-                          width: 2,
-                          color: Colors.deepPurple,
-                        )),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                        borderSide: const BorderSide(
-                            color: Colors.deepPurple, width: 2))),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
-      ),
+            ),
+          );
+        } else {
+          return const Center(
+            child: Text(
+              'Loading....',
+            ),
+          );
+        }
+      },
     );
   }
 }
